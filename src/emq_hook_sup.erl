11ef4661125ed41b41e7a5da0684ae514144e193
hanqingwu <hanqingwu@hengtiansoft.com>
2016-12-16 23:03:44 +0800

    no message

--- src/emq_hook_sup.erl
+++ src/emq_hook_sup.erl
@@ -1,12 +1,16 @@
 -module(emq_hook_sup).
 -behaviour(supervisor).
 
+-include("emq_hook.hrl").
+
 -export([start_link/0]).
 -export([init/1]).
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+    {ok, Server} = application:get_env(?APP, server),
+    RedisPoolSpec = ecpool:pool_spec(?APP, ?APP, emq_redis_cli, Server),
+    Procs = [RedisPoolSpec],
+    {ok, {{one_for_one, 10, 100}, Procs}}.
