49221c21bf344534e7b50a85520b02df2b8fb236
hanqingwu <hanqingwu@hengtiansoft.com>
2016-12-21 01:49:11 +0800

    no message

--- src/emq_kafka_cli.erl
+++ src/emq_kafka_cli.erl
@@ -8,13 +8,17 @@
 
 -define(ENV(Key, Opts), proplists:get_value(Key, Opts)).
 
--export([start/0]).
--export([monitor/1]).
+-export([start_link/0, monitor/1]).
 
 %%--------------------------------------------------------------------
 %% Kafka Connect/Produce
 %%--------------------------------------------------------------------
 
+start_link() ->
+    io.format("start conncet zk"),
+    P = spawn(fun() -> receive ok -> ok end end),
+                                                %monitor(P),
+    {ok, Pid} = erlzk:connect([{"172.16.129.226", 2181}], 30000, [{chroot, "/test"}, {monitor, P}]).
 
 monitor(Pid) ->
     _MonitorRef = erlang:monitor(process, Pid),
@@ -22,9 +26,3 @@ monitor(Pid) ->
         Msg ->
             io:format("pid exit: ~p~n", [Msg])
     end.
-
-start() ->
-    io.format("start conncet zk"),
-    P = spawn(fun() -> receive ok -> ok end end),
-    monitor(P),
-    {ok, Pid} = erlzk:connect([{"172.16.129.226", 2181}], 30000, [{chroot, "/test"}, {monitor, P}]).
