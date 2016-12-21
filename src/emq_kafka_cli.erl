9c814d102f3c9d078bde80ea4aa3099b9264b249
hanqingwu <hanqingwu@hengtiansoft.com>
2016-12-21 17:20:00 +0800

    no message

--- src/emq_kafka_cli.erl
+++ src/emq_kafka_cli.erl
@@ -1,55 +1,141 @@
+%%%-------------------------------------------------------------------
+%%% @author 吴汗青 <wuhanqing@wuhanqingdeMacBook-Pro.local>
+%%% @copyright (C) 2016, 吴汗青
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21 Dec 2016 by 吴汗青 <wuhanqing@wuhanqingdeMacBook-Pro.local>
+%%%-------------------------------------------------------------------
 -module(emq_kafka_cli).
 
-%-behaviour(ecpool_worker).
+-behaviour(gen_server).
 
--include("emq_hook.hrl").
+%% API
+-export([start_link/0]).
 
--include_lib("emqttd/include/emqttd.hrl").
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 
--define(ENV(Key, Opts), proplists:get_value(Key, Opts)).
+-define(SERVER, ?MODULE).
 
--export([start_link/0, dataW/0, childW/0, monitor/1]).
+-record(state, {conn}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
 
 %%--------------------------------------------------------------------
-%% Kafka Connect/Produce
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
 %%--------------------------------------------------------------------
-
 start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    process_flag(trap_exit, true),
     io:format("start conncet zk"),
-    %P = spawn(fun() -> receive ok -> ok end end),
-    %monitor(P),
-
     {ok, Pid} = erlzk:connect([{"172.16.129.226", 2181}], 30000),
-    subscibe_watch(Pid),
-    {ok, Pid}.
+    addWatch(node_data_changed, Pid, "test"),
+    {ok, #state{conn=Pid}}.
 
-subscibe_watch(Pid) ->
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({node_data_changed, Path}, _From, #state{conn=Conn}=State) ->
+    Reply = addWatch(node_data_changed, Conn, Path),
+    {reply, Reply, State}.
 
-    erlzk:get_data(Pid, "/test", spawn_link(dataW())),
-    erlzk:get_children(Pid, "/test", spawn_link(childW())).
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
 
-dataW() ->
-    receive
-        {Event, Path} ->
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+addWatch(node_data_changed, Conn, Path) ->
+    DataChangeWatch = spawn_link(fun() ->
+        receive
+            {Event, Path} ->
             Path = "/test",
             Event = node_data_changed,
-            io:format("node data changed")
-            %erlzk:get_data(Pid, "/test", spawn_link(add(Pid, node_data_changed, "/test")))
-    end.
-childW() ->
-    receive
-        {Event, Path} ->
-            Path = "/test",
-            Event = node_data_changed,
-            io:format("node child changed")
-            %erlzk:get_data(Pid, "/test", spawn_link(add(Pid, node_children_changed, "/test")))
-    end.
-
-
-
-monitor(Pid) ->
-    _MonitorRef = erlang:monitor(process, Pid),
-    receive
-        Msg ->
-            io:format("pid exit: ~p~n", [Msg])
-    end.
+            io:format("node changed")
+        end
+    end),
+    erlzk:get_data(Conn, Path, DataChangeWatch).
